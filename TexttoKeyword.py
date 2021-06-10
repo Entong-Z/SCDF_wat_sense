3296a86acadaeb4ce98d62fe7467f9bb214b99a4
CSYGalvin <77224058+CSYGalvin@users.noreply.github.com>
2021-06-10 17:29:06 +0800

    Update TexttoKeyword.py

--- TexttoKeyword.py
+++ TexttoKeyword.py
@@ -4,12 +4,12 @@ from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
-authenticator = IAMAuthenticator('C3AbU4ad_IPHZJZhwu704gPt7JxWlfjniWpKWRdsIK93')
+authenticator = IAMAuthenticator(APIKEY)
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2020-08-01',
     authenticator=authenticator)
 
-natural_language_understanding.set_service_url('https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/d8779cab-a21c-42f9-9fa2-ec02d291822a')
+natural_language_understanding.set_service_url(URL)
 
 f = open(sys.argv[1], "r")
 
